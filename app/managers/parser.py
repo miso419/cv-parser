80c8338f0d271292234669188101c3da4466de4d
Min Park <miso419@gmail.com>
2020-09-24 09:52:04 +1000

    Implement get_email

--- app/managers/parser.py
+++ app/managers/parser.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from app.managers.nlp_common import get_doc, get_nlp_doc_json
-from app.managers.personal_details import get_name
+from app.managers.personal_details import get_name, get_email
 
 
 def parse(data):
-    doc = get_doc(data['decomposedText'])
+    source_text = data['decomposedText']
+    doc = get_doc(source_text)
     # result = get_nlp_doc_json(doc) -- debug
 
     name = get_name(doc)
-    result = {'name': name}
+    email = get_email(source_text)
+    result = {'name': name, 'email': email}
     return result
--- app/managers/personal_details.py
+++ app/managers/personal_details.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import re
 from app.managers.nlp_common import extract_named_entities
 
 
 def get_name(nlp_doc):
     names = extract_named_entities(nlp_doc, 'PERSON')
     return names[0] if len(names) > 0 else None
+
+
+def get_email(source_text):
+    EMAIL_REGEX = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    pattern = re.compile(EMAIL_REGEX, re.IGNORECASE)
+    emails = re.findall(pattern, source_text)
+    return emails[0] if len(emails) > 0 else None
