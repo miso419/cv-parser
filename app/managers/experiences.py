eb7bb5bfd4bf264746aa146db7fcf3a36af77212
Min Park <miso419@gmail.com>
2020-09-25 13:04:45 +1000

    Draft experiences

--- app/managers/experiences.py
+++ app/managers/experiences.py
@@ -1,18 +1,21 @@
 from app.managers.nlp_common import extract_named_entities, extract_matched
 
 
-def is_institution(value):
-    ins_types = ['univ', 'college', 'institut']
-    for ins_type in ins_types:
-        if ins_type in value.lower():
-            return True
-
-    return False
-
-
 def get_companies(nlp_doc):
     all_orgs = extract_named_entities(nlp_doc, 'ORG')
-    return [org for org in all_orgs if is_institution(org)]
+    new_orgs = []
+    for org in all_orgs:
+        splits = org.split('\n\n\n\n')
+        new_org = org[1] if len(splits) > 0 else org[0]
+        new_orgs.append(new_org)
+
+    return new_orgs
+
+
+def extract_company(orgs, line):
+    matched_org = [org for org in orgs if org in line]
+    institution = matched_org[0] if len(matched_org) > 0 else None
+    return {'institution': institution} if institution else None
 
 
 def get_experiences(nlp_doc, matches):
@@ -23,11 +26,21 @@ def get_experiences(nlp_doc, matches):
     if mat is None:
         return None
 
+    all_orgs = get_companies(nlp_doc)
+
     startIndex = mat['end']
     line = ''
     exp_list = []
 
     for w in nlp_doc[startIndex:]:
-        print(w)
+        # Section
+        if w.shape_ == "\n\n\n\n":
+            nwe_com = extract_company(all_orgs, line)
+            if nwe_com:
+                exp_list.append(nwe_com)
+            line = ''
+            continue
+
+        line = line + ' ' + w.text
 
     return []
